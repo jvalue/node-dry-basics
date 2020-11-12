6406a93cfd0fc1d757339ebe9c268cbcde1e1d30
Georg Schwarz <schwarz@group.riehle.org>
2020-11-12 11:11:37 +0100

    Add functionality for reading env

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,9 +1,11 @@
 import { sleep } from './sleep'
 import * as validators from './validators'
 import * as stringifiers from './stringifiers'
+import { readEnvOrDie } from './env'
 
 export {
   sleep,
   validators,
+  readEnvOrDie,
   stringifiers
 }
